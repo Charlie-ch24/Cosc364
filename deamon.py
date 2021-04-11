bea5347cbcd73040815f463ebce89bedac84aa2f
Bach Vu <bach.vuviet01@gmail.com>
2021-04-11 22:31:34 +1200

    Print routing table

--- deamon.py
+++ deamon.py
@@ -25,6 +25,7 @@ ROUTER = None # Router Obj
 
 ########## Program ##########
 def main():
+    global ROUTER
     try:
         filename = sys.argv[1]
         rID, inputs, outputs = system.read_config(filename)
@@ -35,18 +36,24 @@ def main():
         print("Error: given Config file not found!")
     except ValueError:
         print("Error: Invalid port number in config data")
+    except Exception:
+        raise Exception
+    else:
+        # No exception
+        ROUTER.print_hello()
+    
+    
+if __name__ == "__main__":
+    try:
+        main()
+        while True:
+            ROUTER.print_route_table()
+            time.sleep(3)
+    except KeyboardInterrupt:
+        print("Program exited!")
     except Exception as e:
         traceback.print_exc() # Traceback unknown error
-    else:
-        ROUTER.print_hello()
-
-
-
-
-
+        print(e)
+        print("Program exited unexpectedly.\n")
     finally:
         sys.exit()
-    
-    
-
-main()
--- router.py
+++ router.py
@@ -8,22 +8,41 @@ class Router:
     def __init__(self, rID, inputs, outputs):
         self.ROUTER_ID = rID
         self.INPUT_PORTS = inputs
+
         self.ROUTING_TABLE = {}
-        
+        self.ROUTING_TABLE[rID] = [rID, 0, 0, None]
+
         self.OUTPUT_PORTS = {}
         for output in outputs:
             port, cost, dest = output.split('-')
             self.OUTPUT_PORTS[int(dest)] = (int(port), int(cost))
 
     def print_hello(self):      
-        print("-" * 40)  
+        print("-" * 50)  
         print(f"Router {self.ROUTER_ID} is running ...")
         print("Input ports:", self.INPUT_PORTS)
         print("Output ports:")
         for dest, link in self.OUTPUT_PORTS.items():
             print(f"    {link} to Router ID {dest}")
-        print("-" * 40)
+        print("-" * 50)
+        print("Use Ctrl+C or Del to shutdown.")        
+        print()
 
     def print_route_table(self):
-        pass
+        column = "|{:^10}|{:^10}|{:^10}|{:^10}|{:^20}|"
+        print("="*66)
+        print("{:25}{}{:25}".format(" ", "ROUTING TABLE", " "))
+        print(column.format(
+            "Dest.", "Next Hop", "Cost", "Time", "Path"))
+        print("-"*66)
+        for dest, record in self.ROUTING_TABLE.items():
+            hop, cost, time, path = record
+            print(column.format(dest, hop, cost, time, str(path)))
+
+        print("="*66)
+
+
+
+
+
 
